e6f8a75bf96e69c767c7db6eaad772e1b47e828e
abhi017 <abhinavgarg017@gmail.com>
2016-02-24 00:05:53 +0530

    supervisor searching

--- search_student.php
+++ search_student.php
@@ -123,7 +123,9 @@
 
 
 <?php
-    if((!isset($_GET["regno"]) && !isset($_GET["sname"]) && !isset($_GET["full_part"]) &&  !isset($_GET["status"]) && !isset($_GET["sthesis"]) && !isset($_GET["semail"]) && !isset($_GET["sphone"]) && !isset($_GET["sdob"])) || ( empty($_GET["regno"]) && empty($_GET["sname"]) && empty($_GET["full_part"]) &&  empty($_GET["status"]) && empty($_GET["sthesis"]) && empty($_GET["semail"]) && empty($_GET["sphone"]) && empty($_GET["sdob"]) ) ) {
+
+    if((!isset($_GET["regno"]) && !isset($_GET["supervisor"]) && !isset($_GET["sname"]) && !isset($_GET["full_part"]) &&  !isset($_GET["status"]) && !isset($_GET["sthesis"]) && !isset($_GET["semail"]) && !isset($_GET["sphone"]) && !isset($_GET["sdob"])) || 
+      ( empty($_GET["regno"]) && empty($_GET["supervisor"]) && empty($_GET["sname"]) && empty($_GET["full_part"]) &&  empty($_GET["status"]) && empty($_GET["sthesis"]) && empty($_GET["semail"]) && empty($_GET["sphone"]) && empty($_GET["sdob"]) ) ) {
         echo "<h4>&nbsp;&nbsp;&nbsp;&nbsp;No parameters to search</h4>";
 } else {
         echo "<div class='col-sm-12 bootcards-list' id='list' data-title='students'>";
@@ -152,6 +154,9 @@
         if(!empty($_GET["sdob"]) && isset($_GET["sdob"])) {
             $sql .= " AND sdob='".$_GET["sdob"]."'";
         }
+        if(!empty($_GET["supervisor"]) && isset($_GET["supervisor"])) {
+            $sql .= " AND supervisor1='".$_GET["supervisor"]."' OR supervisor2='".$_GET["supervisor"]."'";
+        }
 
 
     if ($result=mysqli_query($conn,$sql))
@@ -170,7 +175,8 @@
       echo "<p class='list-group-item-text'>&nbsp; Email: "; echo $row["semail"]; echo "</p>";
       echo "<p class='list-group-item-text'>&nbsp; Thesis:"; echo $row["sthesis"]; echo "</p>";
       echo "<p class='list-group-item-text'>&nbsp; Registration No.: "; echo $row["regno"]; echo "</p>";
-      echo "<p class='list-group-item-text'>&nbsp; Supervisor: "; echo $row["supervisor1"]; echo "</p>";
+      echo "<p class='list-group-item-text'>&nbsp; Supervisor: "; echo $row["supervisor1"]; 
+      if($row["supervisor2"]!="NULL"){echo ", ".$row["supervisor2"];}echo "</p>";
       echo "<p class='list-group-item-text'>&nbsp; PhD Stage: "; echo $row["status"]; echo "</p>";
       echo "<p class='list-group-item-text'>&nbsp; Percentage progress: "; echo $rowpro["percentage"]; echo "</p>";
       echo "</a>";
