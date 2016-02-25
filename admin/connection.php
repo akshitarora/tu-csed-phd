e5ce3e3e2f8e98702b1041213b35fff99f591053
Akshit Arora <akshit.arora1995@gmail.com>
2016-02-25 21:36:40 +0530

    connection settings updated

--- admin/connection.php
+++ admin/connection.php
@@ -1,4 +1,4 @@
-<?php $conn = mysqli_connect("localhost", "root", "","phd");
+<?php $conn = mysqli_connect("localhost", "phduser", "password","phd");
 if(mysqli_connect_errno()){
 			header('Refresh:0;URL=/phd/index.php'); die();
 		}
